63a078f5f7d607a03944451f99fcff99404bde70
Tarik Rosin <tarik.rosin@mailbox.org>
2025-11-22 17:22:26 +0100

    fixed procfile

--- src/main.py
+++ src/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello world!"}
+
+
+# example for you guys. replace honesty with your metric
+@app.get("/api/monitor/honesty")
+def monitor_honesty():
+    return {"honesty": 0}
